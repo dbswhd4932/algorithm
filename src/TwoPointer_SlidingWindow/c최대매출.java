58c07bdc4498f5330805bbce01e7f2d90c832c77
jong <dbswhd4932@gmail.com>
2022-10-03 23:09:09 +0900

    추가문제풀이 22.10.03

---
+++ src/TwoPointer_SlidingWindow/c최대매출.java
@@ -0,0 +1,39 @@
+package TwoPointer_SlidingWindow;
+/**
+ * 풀이날짜 22.10.03
+ * 걸린시간 00:25
+ */
+
+import java.util.Scanner;
+
+public class c최대매출 {
+    public static int solution(int n, int k, int[] arr) {
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        answer = sum;
+        for (int i = k; i < n; i++) {
+            sum += (arr[i] - arr[i - k]);
+            answer = Math.max(answer, sum);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(n, k, a));
+
+    }
+}
