e6a47a71722e8a13ca8db2f7d132985b757e661c
yoonjong <dbswhd4932@gmail.com>
2022-11-30 22:58:45 +0900

    22.11.30 재풀이

--- src/greedy/d최대수입스케줄PQ.java
+++ src/greedy/d최대수입스케줄PQ.java
@@ -1,39 +1,37 @@
 package greedy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 class Lecture implements Comparable<Lecture> {
     int money;
-    int time;
+    int date;
 
-    public Lecture(int money, int time) {
+    public Lecture(int money, int date) {
         this.money = money;
-        this.time = time;
+        this.date = date;
     }
 
     @Override
     public int compareTo(Lecture o) {
-        return o.time - this.time;
+        return o.date - this.date;
     }
 }
 
 public class d최대수입스케줄PQ {
 
-    static int n = Integer.MIN_VALUE;
-    static int max = Integer.MIN_VALUE;
+    static int n;
 
     public int solution(ArrayList<Lecture> arr) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         Collections.sort(arr);
+        int max = arr.get(0).date; // 3
+        int answer = 0;
         int j = 0;
-        for (int i = max; i >= 1; i--) {
+
+        for (int i = max; i > 0; i--) {
             for (; j < n; j++) {
-                if (arr.get(j).time < i) break;
-                pq.add(arr.get(j).money);
+                if (arr.get(j).date == i) pq.add(arr.get(j).money);
+                else if (arr.get(j).date < i) break;
             }
             if (!pq.isEmpty()) answer += pq.poll();
         }
@@ -48,9 +46,8 @@ public class d최대수입스케줄PQ {
 
         for (int i = 0; i < n; i++) {
             int money = sc.nextInt();
-            int time = sc.nextInt();
-            arr.add(new Lecture(money, time));
-            if (time > max) max = time;
+            int date = sc.nextInt();
+            arr.add(new Lecture(money, date));
         }
 
         System.out.println(T.solution(arr));
